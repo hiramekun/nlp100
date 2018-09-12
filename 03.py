5a1a1b36ee452fe49d8d0d1c356c23fdf524a600
hiramekun <thescript1210@gmail.com>
2018-09-12 13:37:54 +0900

    Add 03

---
+++ 03.py
@@ -0,0 +1,4 @@
+s1 = 'Now I need a drink, alcohilic of course, after the heavy lectures involving quantum mechanics.'
+s1.replace(',', '').replace('.', '')
+ans = [len(w) for w in s1.split(' ')]
+print(ans)
