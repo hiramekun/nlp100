d21c084c479a9c9f52e64c29eb3bb1bb3d9b1d0b
hiramekun <thescript1210@gmail.com>
2018-09-12 13:42:34 +0900

    Add main

--- 03.py
+++ 03.py
@@ -1,4 +1,5 @@
-s1 = 'Now I need a drink, alcohilic of course, after the heavy lectures involving quantum mechanics.'
-s1.replace(',', '').replace('.', '')
-ans = [len(w) for w in s1.split(' ')]
-print(ans)
+if __name__ == '__main__':
+    s1 = 'Now I need a drink, alcohilic of course, after the heavy lectures involving quantum mechanics.'
+    s1.replace(',', '').replace('.', '')
+    ans = [len(w) for w in s1.split(' ')]
+    print(ans)
