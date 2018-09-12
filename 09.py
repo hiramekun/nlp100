f8c1a29683c78b2e13b97f89591bc5629fb0700a
hiramekun <thescript1210@gmail.com>
2018-09-12 15:03:16 +0900

    Fix

--- 09.py
+++ 09.py
@@ -1,18 +1,12 @@
 import random
 
+
+def shuffle_word(word):
+    shuffled = list(word[1:-1])
+    random.shuffle(shuffled)
+    return word[0] + ''.join(shuffled) + word[-1]
+
+
 if __name__ == '__main__':
     sentence = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind."
-    s_list = sentence.split(' ')
-    idx_lessword = [i for i, s in enumerate(s_list) if len(s) > 4]
-    shuffled = [s for i, s in enumerate(s_list) if i not in idx_lessword]
-    random.shuffle(shuffled)
-    ans = []
-    j = 0
-    for i in range(len(s_list)):
-        if i in idx_lessword:
-            ans.append(s_list[i])
-        else:
-            ans.append(shuffled[j])
-            j += 1
-
-    print(' '.join(ans))
+    print(' '.join([shuffle_word(s) if len(s) > 4 else s for s in sentence.split(' ')]))
