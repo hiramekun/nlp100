5e17441e5e2e7adb6830929727e2c46f4f1002ce
hiramekun <thescript1210@gmail.com>
2018-09-12 15:49:54 +0900

    Add 12

---
+++ 12.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    f1 = open('col1.txt', 'w')
+    f2 = open('col2.txt', 'w')
+    with open('hightemp.txt') as f:
+        for l in f:
+            f1.write(l[0])
+            f2.write(l[1])
+    f1.close()
+    f2.close()
