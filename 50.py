ea14a459d1160eb7e77d5323442370927bd8a92d
hiramekun <thescript1210@gmail.com>
2018-09-13 18:54:58 +0900

    Fix

--- 50.py
+++ 50.py
@@ -1,6 +1,7 @@
 import re
 
-if __name__ == '__main__':
+
+def get_sentence():
     reg = re.compile(r'(.*?[.;:?!])\s([A-Z].*?\.)')
     with open('nlp.txt') as f:
         for l in f:
@@ -9,4 +10,9 @@ if __name__ == '__main__':
             if len(match) > 0:
                 for line in match:
                     if len(line) > 0:
-                        print(line.strip())
+                        yield (line.strip())
+
+
+if __name__ == '__main__':
+    for line in get_sentence():
+        print(line)
