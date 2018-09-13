e7b1da121585d1b89e162850565cc0574f52bc3d
hiramekun <thescript1210@gmail.com>
2018-09-13 14:18:26 +0900

    Add 30

---
+++ 30.py
@@ -0,0 +1,38 @@
+import MeCab
+
+fname = 'neko.txt'
+fname_parsed = 'neko.txt.mecab'
+
+
+def parse_neko():
+    with open(fname) as data_file, open(fname_parsed, 'w') as out_file:
+        mecab = MeCab.Tagger()
+        out_file.write(mecab.parse(data_file.read()))
+
+
+def neco_lines():
+    with open(fname_parsed) as file_parsed:
+        morphemes = []
+        for line in file_parsed:
+            cols = line.split('\t')
+            if len(cols) < 2:
+                raise StopIteration
+            res_cols = cols[1].split(',')
+            morpheme = {
+                'surface': cols[0],
+                'base': res_cols[6],
+                'pos': res_cols[0],
+                'pos1': res_cols[1]
+            }
+            morphemes.append(morpheme)
+
+            if res_cols[1] == '句点':
+                yield morphemes
+                morphemes = []
+
+
+if __name__ == '__main__':
+    parse_neko()
+    lines = neco_lines()
+    for l in lines:
+        print(l)
