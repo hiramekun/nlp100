68d5f0efb8b85b5aebd38aa3db2969d80c6b8ff8
hiramekun <thescript1210@gmail.com>
2018-09-13 17:10:41 +0900

    Fix

--- 70.py
+++ 70.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
 
     with open('sentiment.txt', 'w') as f_write, open(path_neg) as f_neg, open(path_pos) as f_pos:
         sentences = []
-        sentences.extend(['-1 ' + l_neg for l_neg in f_neg])
-        sentences.extend(['+1 ' + l_pos for l_pos in f_pos])
+        lines_f_neg = f_neg.readlines()
+        lines_f_pos = f_pos.readlines()
+        print(f'negative: {len(lines_f_neg)}')
+        print(f'positive: {len(lines_f_pos)}')
+
+        sentences.extend(['-1 ' + l_neg for l_neg in lines_f_neg])
+        sentences.extend(['+1 ' + l_pos for l_pos in lines_f_pos])
         random.shuffle(sentences)
         f_write.write(''.join([line for line in sentences]))
