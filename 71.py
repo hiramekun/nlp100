04f9db31cf4be85dc2ef9cec258deaf3f72e76c8
hiramekun <thescript1210@gmail.com>
2018-09-13 17:27:08 +0900

    Add 71

---
+++ 71.py
@@ -0,0 +1,21 @@
+stop_words = (
+    'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,'
+    'as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,'
+    'either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,'
+    'him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,'
+    'likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,'
+    'on,only,or,other,our,own,rather,said,say,says,she,should,since,so,'
+    'some,than,that,the,their,them,then,there,these,they,this,tis,to,too,'
+    'twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,'
+    'will,with,would,yet,you,your').lower().split(',')
+
+
+def is_stop_word(word):
+    return word.lower() in stop_words
+
+
+if __name__ == '__main__':
+    assert is_stop_word('a')
+    assert is_stop_word('A')
+    assert is_stop_word('you')
+    assert not is_stop_word('fire')
